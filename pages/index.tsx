49469121990934967dcdfdbd37e1cf396d2b2500
Austin Malerba <austin.malerba@gmail.com>
2022-02-11 16:38:07 -0700

    rm code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,9 @@
 import type { NextPage } from "next";
 import * as React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { ChakraProvider, Box } from "@chakra-ui/react";
+import { ChakraProvider, Box, Heading } from "@chakra-ui/react";
 import { Toaster } from "react-hot-toast";
-import Comments from "../components/Comments";
 import theme from "../theme";
-import { Provider as WagmiProvider, InjectedConnector, chain } from "wagmi";
-import { providers } from "ethers";
-
-// Chains for connectors to support
-const chains = [chain.polygonTestnetMumbai, chain.hardhat];
-
-const provider = ({ chainId, connector }) => {
-  const selectedChain = connector?.chains.find((c) => c.id === chainId);
-
-  return providers.getDefaultProvider(
-    selectedChain?.rpcUrls?.[0] || "http://localhost:8545"
-  );
-};
-
-// Set up connectors
-const connectors = () => {
-  return [
-    new InjectedConnector({
-      chains,
-    }),
-  ];
-};
 
 // Create a client
 const queryClient = new QueryClient({
@@ -39,16 +16,14 @@ const queryClient = new QueryClient({
 
 const App: NextPage = () => {
   return (
-    <WagmiProvider autoConnect provider={provider} connectors={connectors}>
-      <ChakraProvider theme={theme}>
-        <QueryClientProvider client={queryClient}>
-          <Box p={8} maxW="600px" minW="320px" m="0 auto">
-            <Comments topic="my-blog-post" />
-            <Toaster position="bottom-right" />
-          </Box>
-        </QueryClientProvider>
-      </ChakraProvider>
-    </WagmiProvider>
+    <ChakraProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <Box p={8} maxW="600px" minW="320px" m="0 auto">
+          <Heading>Oops, no comments yet!</Heading>
+          <Toaster position="bottom-right" />
+        </Box>
+      </QueryClientProvider>
+    </ChakraProvider>
   );
 };
 
