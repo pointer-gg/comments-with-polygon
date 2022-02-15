aeda9593f938209ffe0225554aafdb78157ce8f0
Austin Malerba <austin.malerba@gmail.com>
2022-02-15 08:39:56 -0700

    fix hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,4 +18,9 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
 };
